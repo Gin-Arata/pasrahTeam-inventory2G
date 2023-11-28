3b247a736b43340f4fec72340cabcceb76425952
Gaco Razan Kamil <2241720091@student.polinema.ac.id>
2023-11-29 03:44:22 +0700

    Add admin index and some change in media query

--- app/view/template/headerAdmin.php
+++ app/view/template/headerAdmin.php
@@ -13,9 +13,10 @@
     <!-- CSS Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
     <!-- CSS External -->
     <link rel="stylesheet" href="<?= BASEURL; ?>/css/admin/adminSidebar.css">
-    <link rel="stylesheet" href="<?= BASEURL; ?>/css/admin/adminBody.css">
+    <link rel="stylesheet" href="<?= BASEURL; ?>/css/admin/adminIndex.css">
     <!-- JS Fontawesome -->
     <script src="https://kit.fontawesome.com/b450899c31.js" crossorigin="anonymous"     ></script>
 </head>
@@ -25,32 +26,27 @@
     <div class="sidebar">
         <div class="sidebar-main mt-3">
             <a href="<?= BASEURL2 ?>/admin">
-                <div class="sidebar-content"><img src="<?= BASEURL ?>/img/icon/home.png" alt="Home Logo" width="25"
-                        height="25">
+                <div class="sidebar-content"><img src="<?= BASEURL ?>/img/icon/home.png" alt="Home Logo">
                     <p>Home</p>
                 </div>
             </a>
             <a href="">
-                <div class="sidebar-content"><img src="<?= BASEURL ?>/img/icon/add-file.png" alt="Add barang Logo"
-                        width="25" height="25">
+                <div class="sidebar-content"><img src="<?= BASEURL ?>/img/icon/add-file.png" alt="Add barang Logo">
                     <p>Inventarisir</p>
                 </div>
             </a>
             <a href="">
-                <div class="sidebar-content"><img src="<?= BASEURL ?>/img/icon/to-do.png" alt="List barang Logo"
-                        width="25" height="25">
+                <div class="sidebar-content"><img src="<?= BASEURL ?>/img/icon/to-do.png" alt="List barang Logo">
                     <p>Daftar Barang</p>
                 </div>
             </a>
             <a href="">
-                <div class="sidebar-content"><img src="<?= BASEURL ?>/img/icon/result.png" alt="Home Logo" width="25"
-                        height="25">
+                <div class="sidebar-content"><img src="<?= BASEURL ?>/img/icon/result.png" alt="Home Logo" width="25">
                     <p>Perizinan</p>
                 </div>
             </a>
             <a href="">
-                <div class="sidebar-content"><img src="<?= BASEURL ?>/img/icon/group.png" alt="Home Logo" width="25"
-                        height="25">
+                <div class="sidebar-content"><img src="<?= BASEURL ?>/img/icon/group.png" alt="Home Logo" width="25">
                     <p>Daftar User</p>
                 </div>
             </a>
@@ -58,8 +54,7 @@
 
         <div class="sidebar-footer">
             <a href="<?= BASEURL2 ?>/logout">
-                <div class="sidebar-content"><img src="<?= BASEURL ?>/img/icon/logout.png" alt="Logout logo" width="25"
-                        height="25">
+                <div class="sidebar-content"><img src="<?= BASEURL ?>/img/icon/logout.png" alt="Logout logo">
                     <p>Logout</p>
                 </div>
             </a>
