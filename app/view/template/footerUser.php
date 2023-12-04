8bf37f651144fa29409c9eb2d582f97fb61d1e88
Fitriani Novita Maharani <fitrianinovitamaharani@gmail.com>
2023-12-04 11:27:35 +0700

    Update footerUser.php

--- app/view/template/footerUser.php
+++ app/view/template/footerUser.php
@@ -2,5 +2,30 @@
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script>
+            let sidebar = document.querySelector(".sidebar");
+            let closeBtn = document.querySelector("#btn");
 
+            closeBtn.addEventListener("click", () => {
+                sidebar.classList.toggle("open");
+                menuBtnChange();
+            });
+
+            function menuBtnChange() {
+                if (sidebar.classList.contains("open")) {
+                    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+                } else {
+                    closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+                }
+            }
+
+            // Highlight active page
+            const currentPage = "<?php echo basename($_SERVER['PHP_SELF'], '.php'); ?>";
+            const navLinks = document.querySelectorAll('.nav-list li');
+            navLinks.forEach(link => {
+                if (link.classList.contains(currentPage)) {
+                    link.classList.add('active');
+                }
+            });
+        </script>
 </html>
