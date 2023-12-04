f4d2e3aa7b19b238149c2d28af24f6602a9208c5
Fitriani Novita Maharani <fitrianinovitamaharani@gmail.com>
2023-12-04 11:52:56 +0700

    Update headerUser.php

--- app/view/template/headerUser.php
+++ app/view/template/headerUser.php
@@ -26,35 +26,7 @@
 </head>
 
 <body>
-    <!-- Sidebar -->
-    <div class="sidebar fixed-top">
-        <div class="sidebar-main">
-            <a href="index.php" class="<?= $currentPage === 'Home' ? 'active' : ''; ?>">
-                <div class="sidebar-content"><img src="<?= BASEURL ?>/img/icon/home.png" alt="Home Logo">
-                    <p>Home</p>
-                </div>
-            </a>
-            <a href="MenuPeminjaman.php" class="<?= $currentPage === 'Peminjaman' ? 'active' : ''; ?>">
-                <div class="sidebar-content"><img src="<?= BASEURL ?>/img/icon/add-file.png" alt="Add barang Logo">
-                    <p>Peminjaman</p>
-                </div>
-            </a>
-            <a href="MenuPengembalian.php" class="<?= $currentPage === 'Pengembalian' ? 'active' : ''; ?>">
-                <div class="sidebar-content"><img src="<?= BASEURL ?>/img/icon/to-do.png" alt="List barang Logo">
-                    <p>Pengembalian</p>
-                </div>
-            </a>
-            <a href="History.php" class="<?= $currentPage === 'Histori' ? 'active' : ''; ?>">
-                <div class="sidebar-content"><img src="<?= BASEURL ?>/img/icon/history.png" alt="History Logo">
-                    <p>Histori</p>
-                </div>
-            </a>
-            <a href="Akun.php" class="<?= $currentPage === 'User' ? 'active' : ''; ?>">
-                <div class="sidebar-content"><img src="<?= BASEURL ?>/img/icon/group.png" alt="User Logo" width="25">
-                    <p>User</p>
-                </div>
-            </a>
-        </div>
+
         <section class="home-section">
             <a href="index.php">
                 <div class="text">INVENTARIS JTI POLINEMA</div>
