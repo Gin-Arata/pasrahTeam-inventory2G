940c4fe56e92ec3447608eea5dc6134214cdc9a4
hanasgnt <hanasugianto2006@gmail.com>
2023-12-04 11:11:43 +0700

    header & footer user

---
+++ app/view/template/headerUser.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User's Page</title>
+    <!-- import file php -->
+    <?php
+    include 'userSidebar.php';
+    ?>
+    <!-- Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Secular+One&display=swap" rel="stylesheet">
+    <!-- Bootstrap icon -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    <!-- CSS Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
+    <!-- CSS External -->
+    <link rel="stylesheet" href="<?= BASEURL; ?>/css/user/userSidebar.css">
+    <!-- <link rel="stylesheet" href="<?= BASEURL; ?>/css/user/.css"> -->
+</head>
+
+<body>
