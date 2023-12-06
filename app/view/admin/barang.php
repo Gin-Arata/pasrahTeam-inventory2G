3ad3b4df779b2a5a8f8ef518d89f146b087fcdca
Gaco Razan Kamil <2241720091@student.polinema.ac.id>
2023-12-07 00:02:03 +0700

    Add Edit Barang

--- app/view/admin/barang.php
+++ app/view/admin/barang.php
@@ -24,23 +24,60 @@
                 foreach($data['barang'] as $barang) {
                     ?>
                     <tr>
-                        <td><?= $no; ?></td>
-                        <td>Ini gambar</td>
-                        <td><?= $barang['nama_barang']; ?></td>
-                        <td><?= $barang['kode_barang']; ?></td>
-                        <td><?= $barang['maintainer_barang']; ?></td>
-                        <td><?= $barang['jumlah_barang'] ?></td>
-                        <td><?= $barang['asal_barang'] ?></td>
-                        <td><?= $barang['keterangan_barang'] ?></td>
                         <td>
-                            <a href="" class="btn btn-primary mb-1">Ubah</a>
+                            <?= $no; ?>
+                        </td>
+                        <td>Ini gambar</td>
+                        <td>
+                            <?= $barang['nama_barang']; ?>
+                        </td>
+                        <td>
+                            <?= $barang['kode_barang']; ?>
+                        </td>
+                        <td>
+                            <?= $barang['maintainer_barang']; ?>
+                        </td>
+                        <td>
+                            <?= $barang['jumlah_barang'] ?>
+                        </td>
+                        <td>
+                            <?= $barang['asal_barang'] ?>
+                        </td>
+                        <td>
+                            <?= $barang['keterangan_barang'] ?>
+                        </td>
+                        <td>
+                            <a class="btn btn-primary mb-1" data-bs-toggle="modal" data-bs-target="#modalEditBarang<?= $barang['id_barang'] ?>">Ubah</a>
                             <a href="" class="btn btn-danger">Hapus</a>
                         </td>
                     </tr>
-                <?php 
-                $no++;
+                    <?php
+                    $no++;
                 } ?>
             </tbody>
         </table>
     </div>
 </div>
+
+
+<!-- Modal Edit Barang -->
+<?php foreach($data['barang'] as $dump) { ?>
+    <div class="modal fade" id="modalEditBarang<?= $dump['id_barang']; ?>" tabindex="-1"
+        aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Barang</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <?php var_dump($dump); ?>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php } ?>
