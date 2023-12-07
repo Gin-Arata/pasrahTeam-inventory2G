84436a77b280eaf7142ebd923e1c73f7cb10e388
hanasgnt <hanasugianto2006@gmail.com>
2023-12-07 15:06:24 +0700

    ...

--- app/view/user/menuPengembalian.php
+++ app/view/user/menuPengembalian.php
@@ -9,50 +9,33 @@
                 <h3>Peminjaman >> Form Pengembqliqn Barang </h3>
                 <p>Isilah data peminjaman pada form berikut dan tinjau apakah barang yang akan dipinjam sudah
                     benar. Lalu klik “Pinjam”</p>
-                <form action="" method="POST">
-                    <div class="row">
-                        <div class="input-group mb-3 d-flex align-items-center justify-content-left">
-                            <div class="col-sm-3">
-                                <label class="mb-0 me-2">Nama Peminjam</label>
-                            </div>
-                            <div class="col-md-4">
-                                <input type="text" class="form-control input-set" id="NIP/NIM" placeholder="ex : 1234567890" required>
-                            </div>
-                            <!-- </div> -->
-                        </div>
-                        <div class="input-group mb-3 d-flex align-items-center justify-content-left">
-                            <!-- <div class="row"> -->
-                            <div class="col-sm-3">
-                                <label class="mb-0 me-2">NIP/NIM</label>
-                            </div>
-                            <div class="col-md-4">
-                                <input type="text" class="form-control input-set" id="NIP/NIM" placeholder="ex : 1234567890" required>
-                            </div>
-                        </div>
-                        <div class="input-group mb-3 d-flex align-items-center justify-content-left">
-                            <!-- <div class="row"> -->
-                            <div class="col-sm-3">
-                                <label class=" mb-0 me-2">Mulai pinjam</label>
-                            </div>
-                            <div class="col-md-4">
-                                <input type="date" class="form-control input-set me-2" placeholder="Username" aria-label="Username">
-                            </div>
-                            <div class="col-sm-0">
-                                <label class="mb-0 me-2 ms-5">Sampai</label>
-                            </div>
-                            <div class="col-md-4">
-                                <input type="date" class="form-control input-set" placeholder="Server" aria-label="Server">
-                            </div>
-                        </div>
-                        <div class="input-group mb-3 d-flex align-items-center justify-content-left">
-                            <!-- <div class="row"> -->
-                            <div class="col-sm-3">
-                                <label class="mb-0 me-2">Item yang dipinjam</label>
-                            </div>
-                            <div class="col-md-4">
-                                <input type="text" class="form-control" id="NIP/NIM" placeholder="ex : 1234567890" required>
-                            </div>
-                        </div>
+                <form class="example mr-3 mb-4" action="action_page.php">
+                    <div class="table-responsive">
+                        <table id="tabelPilihBarang" class="table table-striped mt-3" style="width:100%">
+                            <thead>
+                                <tr>
+                                    <th>No.</th>
+                                    <th>Gambar Barang</th>
+                                    <th>Nama Barang</th>
+                                    <th>Kode Barang</th>
+                                    <th>Jumlah Barang</th>
+                                    <th>Keterangan Barang</th>
+                                    <th>Return Barang</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td><?php echo '1'; ?></td>
+                                    <td><?php echo 'APA'; ?></td>
+                                    <td><?php echo 'ApA'; ?></td>
+                                    <td><?php echo 'ApA'; ?></td>
+                                    <?php $value = 4; ?>
+                                    <td><input type="number" value="<?php echo $value; ?>"></td>
+                                    <td><?php echo 'ApA'; ?></td>
+                                    <td><input type="checkbox" id=".." name=".." value=".."></td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                     <div class="d-flex justify-content-center mt-3">
                         <button type="submit" class="btn btn-dark">Submit</button>
